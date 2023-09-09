cbdd40b478e83b3dc190c696ea7b68209f249bb6
SimoniAccioly <67556668+SimoniAccioly@users.noreply.github.com>
2023-09-09 16:15:55 -0300

    Incluindo aplications insights e teste de erro.

--- AulaDevops/Program.cs
+++ AulaDevops/Program.cs
@@ -1,4 +1,10 @@
+using AulaDevops.HealthCheck;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
 
 // Add services to the container.
 
@@ -6,6 +12,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//AddHealth Check
+builder.Services.AddHealthChecks()
+                .AddSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("InfnetPosDb"),
+                    healthQuery: "SELECT 1",
+                    name: "Database",
+                    failureStatus: Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus.Unhealthy
+                )
+                .AddUrlGroup(new Uri("http://httpbin.org/status/200"), "Api Terceiro não autenticada")
+                .AddCheck<HealthCheckRandom>(name: "Api Terceiro Autenticada");
+
+builder.Services.AddHealthChecksUI(s =>
+{
+    s.AddHealthCheckEndpoint("Infnet API", "https://localhost:7089/healthz");
+})
+.AddInMemoryStorage();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
@@ -21,6 +45,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting()
+   .UseEndpoints(config =>
+   {
+       config.MapHealthChecks("/healthz", new HealthCheckOptions
+       {
+           Predicate = _ => true,
+           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+       });
+       config.MapHealthChecksUI();
+   });
 
 app.Run();
 
